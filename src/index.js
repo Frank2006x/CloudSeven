6a42f99dd8631f675f6163cd2892d1fdfc5a03c0
Frank2006x <frank2006x@gmail.com>
2025-03-24 15:19:19 +0530

    Done with welcome page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './styles.css';
+
+const time = document.getElementById('time');
+const date = document.getElementById('date');
+let now = new Date();
+date.innerHTML = now.toDateString();
+
+setInterval(() => {
+  let now = new Date();
+  date.innerHTML = now.toDateString();
+  time.innerHTML = now.toLocaleTimeString().trim();
+}, 1000);
