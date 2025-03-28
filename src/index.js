9d62f41178a30eafbf0ef65e34bae677fa92938e
Frank2006x <frank2006x@gmail.com>
2025-03-28 12:02:53 +0530

    updated stats

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './styles.css';
 
 const time = document.getElementById('time');
 const date = document.getElementById('date');
+const content = document.getElementById('content');
 let now = new Date();
 date.innerHTML = now.toDateString();
 
@@ -36,7 +37,26 @@ async function getData(city) {
     const res = await data.json();
     if (res.success === false) {
       console.log('Invaid City Name');
+      alert('Invalid City Name');
     } else {
+      const windthHumidity = res.current.humidity;
+      console.log(windthHumidity);
+
+      content.innerHTML = '';
+      content.innerHTML = `<div id="details" class="w-100 ">
+
+                  <h1>${res.location.name}</h1>
+                  <h2>${res.location.region}</h2>
+                  <h4>Temperature: ${res.current.temperature} °C</h4>
+                    <h4>Wind speed: ${res.current.wind_speed} Km/hr</h4>
+                    <div class="d-flex flex-row align-items-center ">
+                    <h4 class="me-3">Humidity</h4>
+                      <div class="progress flex-grow-1 " role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar bg-warning border-0" style="width: ${windthHumidity}%"></div>
+                      </div>
+                    </div>
+                    <h4>UV index : ${res.current.uv_index}</h4>
+                  </div>`;
       console.log(res);
     }
   } catch (error) {
